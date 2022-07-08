cb9b52e0467d5516db202dc6bff211c7a83258ab
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-07-08 19:44:53 +0200

    DB context + create models

---
+++ TaskList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.Data
+{
+    class ApplicationDbContextv : DbContext
+    {
+        private string sqlConnectionString = @"C:\USERS\DANIL\SOURCE\REPOS\TASKLIST\TASKLIST\DATA\DATABASETASK.MDF";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(sqlConnectionString);
+        }
+    }
+}
